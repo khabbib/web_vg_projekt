ce94b4dd24381cdb6cff841a95f25ed86de525c3
khabib <habib.pakdel1121@gmail.com>
2024-04-28 11:27:10 +0200

    weather api - current location & city name

---
+++ src/app/service/weather-api.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherApiService {
+  #apiKey = '7fcb103b9d1dbeefdd35f1d670c7512b'; // Replace 'YOUR_API_KEY' with your actual API key from OpenWeatherMap
+  constructor() { }
+
+
+  getWeatherByLocation(lat: number, lon: number) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.#apiKey}`;
+    return axios.get(apiUrl);
+  }
+  
+  getWeatherData(city: string) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.#apiKey}`;
+    return axios.get(apiUrl);
+  }
+}
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,30 +1,55 @@
-import { Component } from '@angular/core';
-import { WeatherApiService } from '../weather-api.service';
+import { Component, OnInit } from '@angular/core';
+import { WeatherApiService } from '../service/weather-api.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.css'
 })
-export class WeatherComponent {
-  city!: string;
-  weather: any;
-  constructor(private weatherApiService: WeatherApiService) {
-    this.weather = '';
-  }
+export class WeatherComponent implements OnInit {
+  weatherData: any;
+  city: string = '';
+  constructor(private weatherApiService: WeatherApiService) {}
 
-  ngOnInit(){}
-  getWeather() {
-    // Send the city 
-    this.weatherApiService.getRandomJoke()
-    .then(res => {
-      console.log(res)
-      this.weather = res.data.joke;
+  ngOnInit(): void {
+    this.getCurrentLocationWeather();
+  }
+  getCurrentLocationWeather() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        this.weatherApiService.getWeatherByLocation(latitude, longitude)
+        .then(response => {
+          this.weatherData = response.data;
+          console.log(this.weatherData);
+        })
+        .catch(error => {
+          console.error('Error fetching weather data:', error);
+        });
+      }, error => {
+        console.error('Error getting location:', error);
+      });
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  }
+  getWeatherByCity() {
+    if (this.city.trim() === '') {
+      console.error('Please enter a city name.');
+      return;
+    }
+    this.weatherApiService.getWeatherData(this.city)
+    .then(response => {
+      this.weatherData = response.data;
+      console.log(this.weatherData);
     })
     .catch(error => {
-      console.log(error);
-    })
+      console.error('Error fetching weather data:', error);
+    });
   }
 }
