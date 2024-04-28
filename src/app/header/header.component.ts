bb905e09753d2b1fa686fdd7395b34bba6a4f57b
khabib <habib.pakdel1121@gmail.com>
2024-04-28 13:14:45 +0200

    header navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,13 +8,44 @@ import { Router } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  active: string = '';
   constructor(private router: Router) {}
+  ngOnInit(): void {
+    // Get the current route
+    const currentRoute = window.location.href;
 
-  navigateHome() {
+    // Determine which navigation link should be marked as active based on the route
+    if (currentRoute.endsWith('/weather')) {
+      this.active = 'Weather';
+    } else if (currentRoute.endsWith('/about')) {
+      this.active = 'About';
+    } else {
+      this.active = 'Home'; // Default to 'home' if the route doesn't match any specific route
+    }
+    // Set the active link and reset the rest
+    this.#setActiveLink();
+  }
+
+  navigateHome(e: MouseEvent) {
+    this.active = "Home";
+    this.#setActiveLink();
     this.router.navigate([""]);
   }
-  navigateWeather() {
+  navigateWeather(e: MouseEvent) {
+    this.active = "Weather";
+    this.#setActiveLink();
     this.router.navigate(["weather"]);
   }
+  #setActiveLink(){
+    const navItems = document.querySelectorAll('.nav-link'); 
+    navItems.forEach(item => {
+      if(item.innerHTML === this.active) {
+        item.classList.add("active");
+      } else {
+        item.classList.remove('active');
+      }
+    });
+  }
+
 }
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -3,6 +3,7 @@ import { WeatherApiService } from '../service/weather-api.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
+type MessageType = {type: 'success' | 'error' | 'default', msg: string};
 @Component({
   selector: 'app-weather',
   standalone: true,
@@ -13,6 +14,7 @@ import { CommonModule } from '@angular/common';
 export class WeatherComponent implements OnInit {
   weatherData: any;
   city: string = '';
+  message: MessageType = {type: "default", msg: ""};
   constructor(private weatherApiService: WeatherApiService) {}
 
   ngOnInit(): void {
@@ -26,21 +28,25 @@ export class WeatherComponent implements OnInit {
         this.weatherApiService.getWeatherByLocation(latitude, longitude)
         .then(response => {
           this.weatherData = response.data;
+          this.message = {type: "success", msg: `Weather for ${response.data.name}`}
           console.log(this.weatherData);
         })
         .catch(error => {
+          this.message = {type: "error", msg: `Weather for ${error}`}
           console.error('Error fetching weather data:', error);
         });
       }, error => {
+        this.message = {type: "error", msg: `Error getting location: ${error}`}
         console.error('Error getting location:', error);
       });
     } else {
+      this.message = {type: "error", msg: `Geolocation is not supported by this browser.`}
       console.error('Geolocation is not supported by this browser.');
     }
   }
   getWeatherByCity() {
     if (this.city.trim() === '') {
-      console.error('Please enter a city name.');
+      alert('Please enter a city name.');
       return;
     }
     this.weatherApiService.getWeatherData(this.city)
@@ -51,5 +57,9 @@ export class WeatherComponent implements OnInit {
     .catch(error => {
       console.error('Error fetching weather data:', error);
     });
+  }
+
+  clearMessage() {
+    this.message = { type: 'default', msg: '' };
   }
 }
