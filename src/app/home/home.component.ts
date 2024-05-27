5c8d9818934110fd7e964835e1e4fbcf6c72361a
Khabib <habib.pakdel1121@gmail.com>
2024-05-27 20:47:40 +0200

    wip - chart

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { WeatherCardComponent } from '../weather-card/weather-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [WeatherCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ src/app/weather-card/weather-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './weather-card.component.html',
+  styleUrl: './weather-card.component.css'
+})
+export class WeatherCardComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
+
